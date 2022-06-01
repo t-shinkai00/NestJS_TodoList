6f3da774665a3af0b9675514afe7b9d322609e01
t-shinkai00 <t.shinkai00@gmail.com>
2022-06-01 23:30:04 +0900

    add controller, service, and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskController } from './task/task.controller';
+import { TaskService } from './task/task.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(), TaskModule],
+  controllers: [AppController, TaskController],
+  providers: [AppService, TaskService],
 })
 export class AppModule {}
